4827389bbc5b8d084fe6ef5adc1ff899eaccdb31
Nikunj <nikunj.singh@live.com>
2025-03-20 22:22:41 -0400

    grid layout

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,13 +1,13 @@
 function Grid() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      <div className="bg-gray-400 rounded-lg min-h-[50px] "></div>
-      <div className="bg-gray-500 rounded-lg min-h-[50px] row-span-2"></div>
-      <div className="bg-gray-600 rounded-lg min-h-[50px] "></div>
-      <div className="bg-gray-700 rounded-lg min-h-[50px] "></div>
-      <div className="bg-gray-800 rounded-lg min-h-[50px] "></div>
-      <div className="bg-gray-400 rounded-lg min-h-[50px] "></div>
-      <div className="bg-gray-500 rounded-lg min-h-[50px] "></div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-4">
+      <div className="bg-gray-400 rounded-lg min-h-[200px] "></div>
+      <div className="bg-gray-500 rounded-lg min-h-[200px] row-span-2"></div>
+      <div className="bg-gray-600 rounded-lg min-h-[200px] "></div>
+      <div className="bg-gray-700 rounded-lg min-h-[200px] "></div>
+      <div className="bg-gray-800 rounded-lg min-h-[200px] "></div>
+      <div className="bg-gray-400 rounded-lg min-h-[200px] "></div>
+      <div className="bg-gray-500 rounded-lg min-h-[200px] "></div>
     </div>
   );
 }
