69e5ab8118718bbf9ad1d17b0ab80d64624c5eec
Nikunj <nikunj.singh@live.com>
2025-03-20 22:28:47 -0400

    grid responsive fully working

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,6 @@
 function Grid() {
   return (
+    <div className="h-[calc(100vh-5rem)] overflow-auto">
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-4">
       <div className="bg-gray-400 rounded-lg min-h-[200px] "></div>
       <div className="bg-gray-500 rounded-lg min-h-[200px] row-span-2"></div>
@@ -8,6 +9,7 @@ function Grid() {
       <div className="bg-gray-800 rounded-lg min-h-[200px] "></div>
       <div className="bg-gray-400 rounded-lg min-h-[200px] "></div>
       <div className="bg-gray-500 rounded-lg min-h-[200px] "></div>
+    </div>
     </div>
   );
 }
