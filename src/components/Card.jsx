5deb6d8e161ec11a247757f64b85adc1117a6bac
Nikunj <nikunj.singh@live.com>
2025-03-21 00:18:13 -0400

    clean code to arrays

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,85 @@
+import { PackageOpen } from "lucide-react";
+
 function Card() {
+  const cardList = [
+    {
+      title: "Orders",
+      value: "142,293",
+      logo: <PackageOpen size={20} />,
+      rowspan: "",
+      minh: "150px",
+    },
+    {
+      title: "Monthly Deals",
+      value: "2,235",
+      logo: <PackageOpen size={20} />,
+      rowspan: "row-span-2",
+      minh: "300px",
+    },
+    {
+      title: "New Orders",
+      value: "42,495",
+      logo: <PackageOpen size={20} />,
+      rowspan: "",
+      minh: "150px",
+    },
+    {
+      title: "Total Orders",
+      value: "$24,875",
+      logo: <PackageOpen size={20} />,
+      rowspan: "",
+      minh: "150px",
+    },
+    {
+      title: "Total Revenue",
+      value: "$52,128",
+      logo: <PackageOpen size={20} />,
+      rowspan: "",
+      minh: "150px",
+    },
+    {
+      title: "Conversion Rate",
+      value: "56%",
+      logo: <PackageOpen size={20} />,
+      rowspan: "",
+      minh: "150px",
+    },
+    {
+      title: "Bounce Rate",
+      value: "23%",
+      logo: <PackageOpen size={20} />,
+      rowspan: "",
+      minh: "150px",
+    },
+    {
+        title: "Views",
+        value: "101,321",
+        logo: <PackageOpen size={20} />,
+        rowspan: "",
+        minh: "150px",
+      },
+  ];
 
-    return(
-        <div>Card</div>
-    )
+  return (
+    <>
+      {cardList.map((card, index) => (
+        <div
+          key={index}
+          className={`border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[${card.minh}] ${card.rowspan}`}
+        >
+          <div className="flex">
+            <div className="pl-10 pt-10 text-white text-xl font-normal">
+              {card.title}
+            </div>
+            <div className="pl-2 pt-11 text-white">{card.logo}</div>
+          </div>
 
+          <div className="pl-10 text-white text-2xl font-semibold">
+            {card.value}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 export default Card;
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,64 +5,7 @@ function Grid() {
   return (
     <div className="h-[calc(100vh-5rem)] overflow-auto">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-4">
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[150px] ">
-          <div className="flex">
-            <div className="pl-10 pt-10 text-white text-xl font-normal">
-              Orders
-            </div>
-            <div className="pl-2 pt-11 text-white">
-              <PackageOpen size={20} />
-            </div>
-          </div>
-
-          <div className="pl-10 text-white text-2xl font-semibold">142,293</div>
-        </div>
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[300px] row-span-2">
-          <div className="pl-10 pt-10 text-white text-xl font-normal">
-            Monthly Deals
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">2,234</div>
-          <div className="pl-10 pt-25 text-white text-xl font-normal">
-            Server Performance
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">57%</div>
-        </div>
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[150px] ">
-          <div className="pl-10 pt-10 text-white text-xl font-normal">
-            New Orders
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">41,495</div>
-        </div>
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[150px] ">
-          <div className="pl-10 pt-10 text-white text-xl font-normal">
-            Total Orders
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">$24,879</div>
-        </div>
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[150px] ">
-          <div className="pl-10 pt-10 text-white text-xl font-normal">
-            Total Revenue
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">$52,127</div>
-        </div>
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[150px] ">
-          <div className="pl-10 pt-10 text-white text-xl font-normal">
-            Total Revenue
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">$52,127</div>
-        </div>
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[150px] ">
-          <div className="pl-10 pt-10 text-white text-xl font-normal">
-            Conversion Rate
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">56%</div>
-        </div>
-        <div className=" border border-[rgba(255,255,255,0.3)] rounded-lg min-h-[150px] ">
-          <div className="pl-10 pt-10 text-white text-xl font-normal">
-            Churn Rate
-          </div>
-          <div className="pl-10 text-white text-2xl font-semibold">23%</div>
-        </div>
+        <Card />
       </div>
     </div>
   );
